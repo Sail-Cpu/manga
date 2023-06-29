5799144a855d1f142472b8297a423f6d272b349e
sofiane lasoa <sofiane.lasoa.etu@univ-lille.fr>
2023-06-29 15:15:29 +0200

    Update Collection Page

--- client/src/components/other/Stars.js
+++ client/src/components/other/Stars.js
@@ -2,19 +2,19 @@ import React from "react";
 //icons
 import StarBorderIcon from "@mui/icons-material/StarBorder";
 
-const Stars = () => {
-  return (
-    <div className="stars-container">
-      <div className="stars">
-        <StarBorderIcon />
-        <StarBorderIcon />
-        <StarBorderIcon />
-        <StarBorderIcon />
-        <StarBorderIcon />
-      </div>
-      <span>4.3</span>
-    </div>
-  );
+const Stars = (props) => {
+    return (
+        <div className="stars-container">
+            <div className="stars">
+                <StarBorderIcon/>
+                <StarBorderIcon/>
+                <StarBorderIcon/>
+                <StarBorderIcon/>
+                <StarBorderIcon/>
+            </div>
+            <span>{props.critic}</span>
+        </div>
+    );
 };
 
 export default Stars;
