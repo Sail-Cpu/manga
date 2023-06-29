ca505b871575455134316ea0139e08d07771e5a1
sofiane lasoa <sofiane.lasoa.etu@univ-lille.fr>
2023-06-29 22:57:12 +0200

    Implement Stars feature

--- client/src/components/other/Stars.js
+++ client/src/components/other/Stars.js
@@ -1,16 +1,38 @@
 import React from "react";
 //icons
 import StarBorderIcon from "@mui/icons-material/StarBorder";
+import StarIcon from "@mui/icons-material/Star";
 
 const Stars = (props) => {
+
+    function rating(critic) {
+        let stars = [];
+        for (let i = 0; i < 5; i++) {
+            if (critic >= i + 1) {
+                stars.push(0);
+            } else if (critic < i + 1) {
+                let l = parseInt(critic.toString().split('.')[1][0]);
+                stars.push((100 - l * 10));
+            } else {
+                stars.push(100);
+            }
+        }
+        console.log(stars)
+        return stars;
+    }
+
+
     return (
         <div className="stars-container">
             <div className="stars">
-                <StarBorderIcon/>
-                <StarBorderIcon/>
-                <StarBorderIcon/>
-                <StarBorderIcon/>
-                <StarBorderIcon/>
+                {rating(props.critic).map(rat => {
+                    return (
+                        <div className="star">
+                            <StarBorderIcon className="star-border"/>
+                            <StarIcon className="star-no-border" style={{clipPath: `inset(0 ${rat}% 0 0)`}}/>
+                        </div>
+                    )
+                })}
             </div>
             <span>{props.critic}</span>
         </div>
