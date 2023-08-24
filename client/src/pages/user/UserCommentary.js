bdf5247411c11f6515b7b7e0a48d58448049adde
sofiane lasoa <sofiane.lasoa.etu@univ-lille.fr>
2023-08-24 15:33:21 +0200

    Update Responsive

--- client/src/pages/user/UserCommentary.js
+++ client/src/pages/user/UserCommentary.js
@@ -1,98 +1,101 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
+import React, {useContext, useEffect, useRef, useState} from "react";
 //Api
-import { get } from "../../api/ApiManga";
-import { User } from "../../api/ApiUser";
+import {get} from "../../api/ApiManga";
+import {User} from "../../api/ApiUser";
 //Context
-import { UserContext } from "../../context/UserContext";
+import {UserContext} from "../../context/UserContext";
 //Components
 import UserComment from "../../components/other/UserComment";
 
 const UserCommentary = () => {
-  const { getToken } = useContext(UserContext);
-  const userCommentaryRef = useRef();
-  const [commentaryColumn, setCommentaryColumn] = useState({
-    width: 0,
-    number: 0,
-  });
-  const minSize = 450;
-
-  useEffect(() => {
-    if (userCommentaryRef.current) {
-      setCommentaryColumn({
-        width: userCommentaryRef.current.offsetWidth,
-        number: parseInt(userCommentaryRef.current.offsetWidth / minSize),
-      });
-    }
-  }, [userCommentaryRef.current]);
-
-  const handleResize = () => {
-    setCommentaryColumn({
-      width: userCommentaryRef.current.offsetWidth,
-      number: parseInt(userCommentaryRef.current.offsetWidth / minSize),
+    const {getToken} = useContext(UserContext);
+    const userCommentaryRef = useRef();
+    const [commentaryColumn, setCommentaryColumn] = useState({
+        width: 0,
+        number: 0,
     });
-  };
+    const minSize = 450;
 
-  useEffect(() => {
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  const [allComment, setAllComment] = useState([]);
-
-  useEffect(() => {
-    User.fetchUserById(getToken()?.id)
-      .then((response) => {
-        let mangaComment = response.mangaComment;
-        for (let i = 0; i < mangaComment.length; i++) {
-          get
-            .fetchMangasById(mangaComment[i].manga_Id)
-            .then((response) => {
-              setAllComment((comment) => [
-                ...comment,
-                { data: response.data[0], comment: mangaComment[i] },
-              ]);
-            })
-            .catch((error) => {
-              console.log(error);
+    useEffect(() => {
+        if (userCommentaryRef.current && commentaryColumn.number !== 1) {
+            setCommentaryColumn({
+                width: userCommentaryRef.current.offsetWidth,
+                number: parseInt(userCommentaryRef.current.offsetWidth / minSize),
             });
         }
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
+    }, [userCommentaryRef.current]);
 
-  const columns = Array.from({ length: commentaryColumn.number });
+    const handleResize = () => {
+        if (commentaryColumn.number !== 1) {
+            setCommentaryColumn({
+                width: userCommentaryRef.current.offsetWidth,
+                number: parseInt(userCommentaryRef.current.offsetWidth / minSize),
+            });
+        }
 
-  return (
-    <div
-      ref={userCommentaryRef}
-      className="user-commentary-container"
-      style={{
-        gridTemplateColumns: `repeat(${commentaryColumn.number}, ${minSize}px)`,
-      }}
-    >
-      {columns.map((_, idx) => (
-        <div key={idx} className="user-commentary-column">
-          {allComment
-            .filter((_, i) => i % commentaryColumn.number === idx)
-            .map((comment, i) => (
-              <UserComment
-                collect_id={comment.data.collection_id}
-                poster={comment.data.poster}
-                name={comment.data.name}
-                title={comment.comment.title}
-                commentary={comment.comment.commentary}
-                comment_id={comment.comment.comment_id}
-                key={i}
-              />
+    };
+
+    useEffect(() => {
+        window.addEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
+    const [allComment, setAllComment] = useState([]);
+
+    useEffect(() => {
+        User.fetchUserById(getToken()?.id)
+            .then((response) => {
+                let mangaComment = response.mangaComment;
+                for (let i = 0; i < mangaComment.length; i++) {
+                    get
+                        .fetchMangasById(mangaComment[i].manga_Id)
+                        .then((response) => {
+                            setAllComment((comment) => [
+                                ...comment,
+                                {data: response.data[0], comment: mangaComment[i]},
+                            ]);
+                        })
+                        .catch((error) => {
+                            console.log(error);
+                        });
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+
+    const columns = Array.from({length: commentaryColumn.number !== 0 ? commentaryColumn.number : 1});
+
+    return (
+        <div
+            ref={userCommentaryRef}
+            className="user-commentary-container"
+            style={{
+                gridTemplateColumns: `repeat(${columns.length}, minmax(250px, ${minSize}px)`,
+            }}
+        >
+            {columns.map((_, idx) => (
+                <div key={idx} className="user-commentary-column">
+                    {allComment
+                        .filter((_, i) => i % columns.length === idx)
+                        .map((comment, i) => (
+                            <UserComment
+                                collect_id={comment.data.collection_id}
+                                poster={comment.data.poster}
+                                name={comment.data.name}
+                                title={comment.comment.title}
+                                commentary={comment.comment.commentary}
+                                comment_id={comment.comment.comment_id}
+                                key={i}
+                            />
+                        ))}
+                </div>
             ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 };
 
 export default UserCommentary;
